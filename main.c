9da70df2c21911bdc09a7d2ada57ac0a29fa8a9d
eZpZlmnsqwyZ <fiftytrue@gmail.com>
2019-05-08 11:34:33 +0700

    very last version

--- main.c
+++ main.c
@@ -4,13 +4,9 @@
 #include <sys/uio.h>
 
 int main(void){
-<<<<<<< HEAD
 	pritnf("kuy fill");
-	printf("Y");
-=======
 	printf("kuy rai sus");
 	printf("x");
->>>>>>> 01ffbd5fadfecbd9edc9331063d00a17ad7d114b
 	printf("irt;hnsrtihnurtohrtihnt");
 	printf("testt");
 	return 0; 	
