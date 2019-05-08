06846d75495b773ebb57b4e9ee73d47d21525143
eZpZlmnsqwyZ <fiftytrue@gmail.com>
2019-05-08 11:38:45 +0700

    branchinggg

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 int main(void){
 	pritnf("kuy fill");
 	printf("kuy rai sus");
-	printf("xdjkltydh ");
+	printf("this is a new branch!");
 	printf("irt;hnsrtihnurtohrtihnt");
 	printf("testt");
 	return 0; 	
